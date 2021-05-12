a7017ee4ce3ee456b62913ff4a0843078637e678
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-12 15:56:05 -0700

    Branch 01 (antallaModulos): UI de primer parte de la pantalla de iniciar sesión (Imagen logo + botones "Iniciar sesión" y "Registro" )

--- PlatziGameStream/ContentView.swift
+++ PlatziGameStream/ContentView.swift
@@ -7,15 +7,131 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            
+            Color(red: 18/255, green: 31/255, blue: 61/255, opacity: 100).ignoresSafeArea()
+            
+            VStack{
+                
+                Spacer()
+                
+               Image("AppLogo").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250)
+                    .padding(.bottom, 60.0)
+         
+                
+                InicioYRegistroView()
+                
+            }
+            
+        }
     }
+}
+
+
+struct InicioYRegistroView:View {
+    
+    @State var tipoInicioSesion:Bool = true
+    
+    var body: some View{
+        
+        VStack{
+            
+            HStack{
+                
+                Spacer()
+                
+                Button("INICIAR SESIÓN"){
+                    
+                    print("Pantalla Inicio sesion")
+                    tipoInicioSesion = true
+                }
+                .foregroundColor(tipoInicioSesion ? .white : .gray)
+                
+                Spacer()
+                
+                Button("REGÍSTRATE"){
+                    
+                    print("Pantalla Registro")
+                    tipoInicioSesion = false
+                }.foregroundColor(tipoInicioSesion ? .gray : .white)
+                
+                Spacer()
+            }
+            
+            
+            Spacer(minLength: 42)
+            
+            
+            if(tipoInicioSesion){
+                InicioSesiónView()
+            }else{
+                RegistroView()
+            }
+            
+        }
+        
+    }
+}
+
+struct InicioSesiónView: View {
+    
+   
+    var body: some View {
+        
+        
+      
+        Text("Pantalla de Inicio de Sesión")
+            .foregroundColor(Color.white)
+            .padding()
+        
+    }
+    
+    
+    
+    
+    
+}
+
+struct RegistroView: View {
+    var body: some View {
+        
+       
+            Text("Pantalla de Registro")
+                .foregroundColor(Color.white)
+                .padding()
+        
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
+        
     }
 }
+
+
+/*Branches
+ //1.- Branch 01 (pantallaModulos): UI de primer parte de la pantalla de iniciar sesion (Imagen logo + botones "Iniciar sesion" y "Registro" )
+ 
+
+ 
+ 
+
+ 
+ 
+ 
+ */
+
