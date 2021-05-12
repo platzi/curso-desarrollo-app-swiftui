e2514c096430d37d5090cccdbbec8156c2572b9d
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-12 16:01:36 -0700

    Branch 02 (pantallaInicioSesion): UI completo de pantalla de Inicio de Sesión

--- PlatziGameStream/ContentView.swift
+++ PlatziGameStream/ContentView.swift
@@ -84,19 +84,110 @@ struct InicioYRegistroView:View {
 
 struct InicioSesiónView: View {
     
-   
+    @State var correo:String = ""
+    @State var contraseña:String = ""
+    
     var body: some View {
         
         
-      
-        Text("Pantalla de Inicio de Sesión")
-            .foregroundColor(Color.white)
-            .padding()
+        ScrollView{
+            
+            VStack(alignment: .leading){
+                
+                Text("Correo electrónico")
+                    .foregroundColor(Color(red: 63/255, green: 202/255, blue: 160/255, opacity: 1.0))
+                
+                ZStack(alignment: .leading){
+                    if correo.isEmpty { Text("ejemplo@gmail.com").font(.caption).foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0)) }
+                    
+                    TextField("", text: $correo)
+                }
+                
+                Divider()
+                    .frame(height: 1)
+                    .background(Color("Dark-Cyan")).padding(.bottom)
+                
+                
+                Text("Contraseña").foregroundColor(.white)
+                
+                
+                ZStack(alignment: .leading){
+                    if contraseña.isEmpty { Text("Introduce tu contraseña").font(.caption).foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0)) }
+                    
+                    SecureField("", text: $contraseña)
+                    
+                }
+                
+                Divider()
+                    .frame(height: 1)
+                    .background(Color("Dark-Cyan"))
+                
+                Text("¿Olvidaste tu contraseña?")
+                    .font(.footnote)
+                    .frame(width: 300,  alignment: .trailing)
+                    .foregroundColor(Color("Dark-Cyan"))
+                    .padding(.bottom)
+                
+                Button(action: iniciarSesion) {
+                    Text("Iniciar Sesión")
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame( maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color("Dark-Cyan"), lineWidth: 3).shadow(color: .white, radius: 6))
+                }.padding(.bottom)
+                
+                
+                
+
+                
+                
+                Text("Iniciar sesión con redes sociales").frame( maxWidth: .infinity,  alignment: .center).foregroundColor(.white)
+                
+                
+                HStack {
+                        
+    
+                        Button(action: {print("Inicio de sesión con Facebook")}) {
+                            Text("Facebook")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .padding(.vertical, 3.0)
+                                .frame( maxWidth: .infinity, alignment: .center)
+                                .background(Color("Blue-Gray"))
+                                .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                        }
+                        
+                        .padding()
+                    
+                    
+                    
+                    Button(action: {print("Inicio de sesión con Twitter")}) {
+                        Text("Twitter")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 3.0)
+                            .frame( maxWidth: .infinity, alignment: .center)
+                            .background(Color("Blue-Gray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                    }.padding()
+                    
+                }
+                
+            }.padding(.horizontal, 77.0)
+        }
+        
+
         
     }
     
     
-    
+    func iniciarSesion() {
+        print("Mi correo es \(correo) y mi contraseña es \(contraseña)")
+    }
     
     
 }
@@ -126,12 +217,16 @@ struct ContentView_Previews: PreviewProvider {
 /*Branches
  //1.- Branch 01 (pantallaModulos): UI de primer parte de la pantalla de iniciar sesion (Imagen logo + botones "Iniciar sesion" y "Registro" )
  
-
+ //2.- Branch 02 (pantallaInicioSesion): UI de pantalla de Inicio de Sesión completa
  
  
-
+ 
+ 
  
  
  
  */
+
+
+
 
