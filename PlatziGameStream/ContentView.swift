5942408c79ab72ffd2f079b2786c5bc2eed408aa
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-12 15:25:41 -0700

    Initial Commit

---
+++ PlatziGameStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlatziGameStream
+//
+//  Created by Juan Villalvazo on 12/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlatziGameStream/PlatziGameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziGameStreamApp.swift
+//  PlatziGameStream
+//
+//  Created by Juan Villalvazo on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PlatziGameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
