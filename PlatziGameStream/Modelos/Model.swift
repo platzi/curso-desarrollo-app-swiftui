5c0299af7f1f82072c18e7aa77e93aa734a8d874
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-14 00:05:04 -0700

    beta branch 05

--- PlatziGameStream/Modelos/Model.swift
+++ PlatziGameStream/Modelos/Model.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+
+//Cumplimos con el protocolo Codable para poder codificar y decodificar objetos JSON a tipos que swift pueda comprender y utilizar.
+
+struct Games:Codable {
+    var games:[Game]
+}
+
+
+struct Game:Codable {
+    
+    
+    var title:String
+    var studio:String
+    var contentRaiting:String
+    var publicationYear:String
+    var description:String
+    var platforms:[String]
+    var tags:[String]
+    var videosUrls:videoUrl
+    var galleryImages:[String]
+        
+    
+}
+
+
+struct videoUrl:Codable {
+   
+    var mobile:String
+    var tablet:String
+    
+}
--- PlatziGameStream/Vistas/GamesView.swift
+++ PlatziGameStream/Vistas/GamesView.swift
@@ -23,3 +23,4 @@ struct GamesView:View {
         
     }
 }
+
