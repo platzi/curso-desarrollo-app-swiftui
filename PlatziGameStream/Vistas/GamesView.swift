ec04722ce773a6baa47018cfa3b48e454ac881ae
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-13 23:58:55 -0700

    Beta branch 05

---
+++ PlatziGameStream/Vistas/GamesView.swift
@@ -0,0 +1,25 @@
+//
+//  GamesView.swift
+//  PlatziGameStream
+//
+//  Created by Juan Villalvazo on 13/05/21.
+//
+
+import SwiftUI
+
+struct GamesView:View {
+    
+    @ObservedObject var todosLosVideojuegos = ViewModel()
+    
+    var body: some View{
+        
+        
+        Text("Hola desde GamesView").navigationBarHidden(true).navigationBarBackButtonHidden(true).onAppear(perform: {
+            
+            //Muestra la información del primer elemento del json
+            print("Primer elemento del json: \(todosLosVideojuegos.gamesInfo[0])")
+            print("Titulo del primer elemento del json: \(todosLosVideojuegos.gamesInfo[0].title)")
+        })
+        
+    }
+}
---
+++ PlatziGameStream/VistasModelos/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  PlatziGameStream
+//
+//  Created by Juan Villalvazo on 13/05/21.
+//
+
+import Foundation
