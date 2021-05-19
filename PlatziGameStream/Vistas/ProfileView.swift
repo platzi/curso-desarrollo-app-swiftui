872474100967111f4d3ef1f8bc045253d68b4dd8
Juan Villalvazo <villalvazolive@gmail.com>
2021-05-18 21:58:02 -0700

    beta branch perfil usuario

---
+++ PlatziGameStream/Vistas/ProfileView.swift
@@ -0,0 +1,127 @@
+//
+//  ProfileView.swift
+//  PlatziGameStream
+//
+//  Created by Juan Villalvazo on 18/05/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    
+    
+    
+    
+    var body: some View {
+       
+            
+        ZStack {
+            
+            Color("Marine").ignoresSafeArea().navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+            
+            VStack{
+                    
+                  
+                    VStack{
+                       
+                        
+                       
+                          
+                                Image("perfilEjemplo").resizable().aspectRatio(contentMode: .fit).frame(width: 80.0, height: 80.0)
+               
+                        Text("Lorem")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            
+
+                    }.padding(.vertical, 18)
+                
+               
+                    Text("Ajustes")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white).frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity,  alignment: .leading).padding(.leading,18)
+                
+           
+                ModuloAjustes()
+             
+                Spacer()
+            }
+            
+         
+        }
+   }
+    
+}
+
+
+struct ModuloAjustes:View {
+    
+    @State var isToggleOn = true
+    @State var isEditProfileViewActive = false
+    
+    var body: some View{
+        
+        
+        VStack{
+                 
+            Button(action: {}, label: {
+                    HStack { Text("Cuenta")
+                    .foregroundColor(Color.white)
+                    Spacer()
+                    Text(">")
+                        .foregroundColor(Color.white)}.padding()
+            }) .background(Color("Blue-Gray"))
+            .clipShape(RoundedRectangle(cornerRadius: 1.0)).padding(.horizontal, 8.0)
+            
+            Button(action: {}, label: {
+                    HStack { Text("Notificaciones")
+                    .foregroundColor(Color.white)
+                    Spacer()
+                    
+                        Toggle("", isOn: $isToggleOn)
+                    
+                    }.padding()
+            }) .background(Color("Blue-Gray"))
+            .clipShape(RoundedRectangle(cornerRadius: 1.0)).padding(.horizontal, 8.0)
+            
+            Button(action: {isEditProfileViewActive = true}, label: {
+                    HStack { Text("Editar Perfil")
+                    .foregroundColor(Color.white)
+                    Spacer()
+                    Text(">")
+                        .foregroundColor(Color.white)}.padding()
+            }) .background(Color("Blue-Gray"))
+            .clipShape(RoundedRectangle(cornerRadius: 1.0)).padding(.horizontal, 8.0)
+            
+           
+            Button(action: {}, label: {
+                    HStack { Text("Califica esta aplicación")
+                    .foregroundColor(Color.white)
+                    Spacer()
+                    Text(">")
+                        .foregroundColor(Color.white)}.padding()
+            }) .background(Color("Blue-Gray"))
+            .clipShape(RoundedRectangle(cornerRadius: 1.0)).padding(.horizontal, 8.0)
+                
+                
+            NavigationLink(
+                destination: EditProfileView()
+                ,
+                isActive: $isEditProfileViewActive,
+                label: {
+                    EmptyView()
+                })
+            
+        }
+        
+        
+    }
+}
+
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
