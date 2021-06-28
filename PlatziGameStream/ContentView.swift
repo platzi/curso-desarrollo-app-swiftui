03cffea466c6a8daaa945fbecb34d4875328c28f
Juan Villalvazo <villalvazolive@gmail.com>
2021-06-28 13:40:48 -0700

    Branch PantallaInicioSesionConSecureFieldScroll

--- PlatziGameStream/ContentView.swift
+++ PlatziGameStream/ContentView.swift
@@ -112,7 +112,7 @@ struct InicioSesiónView: View {
                 
                 
                 ZStack(alignment: .leading){
-                    if contraseña.isEmpty { Text("Introduce tu contraseña").font(.caption).foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0)) }
+                    if contraseña.isEmpty { Text("Escribe tu contraseña").font(.caption).foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0)) }
                     
                     SecureField("", text: $contraseña)
                     
@@ -122,60 +122,8 @@ struct InicioSesiónView: View {
                     .frame(height: 1)
                     .background(Color("Dark-Cyan"))
                 
-                Text("¿Olvidaste tu contraseña?")
-                    .font(.footnote)
-                    .frame(width: 300,  alignment: .trailing)
-                    .foregroundColor(Color("Dark-Cyan"))
-                    .padding(.bottom)
+               
                 
-                Button(action: iniciarSesion) {
-                    Text("Iniciar Sesión")
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .frame( maxWidth: .infinity, alignment: .center)
-                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
-                        .overlay(RoundedRectangle(cornerRadius: 6)
-                                    .stroke(Color("Dark-Cyan"), lineWidth: 3).shadow(color: .white, radius: 6))
-                }.padding(.bottom)
-                
-                
-                
-
-                
-                
-                Text("Iniciar sesión con redes sociales").frame( maxWidth: .infinity,  alignment: .center).foregroundColor(.white)
-                
-                
-                HStack {
-                        
-    
-                        Button(action: {print("Inicio de sesión con Facebook")}) {
-                            Text("Facebook")
-                                .font(.subheadline)
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
-                                .padding(.vertical, 3.0)
-                                .frame( maxWidth: .infinity, alignment: .center)
-                                .background(Color("Blue-Gray"))
-                                .clipShape(RoundedRectangle(cornerRadius: 4.0))
-                        }
-                        
-                        .padding()
-                    
-                    
-                    
-                    Button(action: {print("Inicio de sesión con Twitter")}) {
-                        Text("Twitter")
-                            .font(.subheadline)
-                            .fontWeight(.bold)
-                            .foregroundColor(.white)
-                            .padding(.vertical, 3.0)
-                            .frame( maxWidth: .infinity, alignment: .center)
-                            .background(Color("Blue-Gray"))
-                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
-                    }.padding()
-                    
-                }
                 
             }.padding(.horizontal, 77.0)
         }
@@ -184,10 +132,7 @@ struct InicioSesiónView: View {
         
     }
     
-    
-    func iniciarSesion() {
-        print("Mi correo es \(correo) y mi contraseña es \(contraseña)")
-    }
+   
     
     
 }
@@ -197,8 +142,8 @@ struct RegistroView: View {
         
        
             Text("Pantalla de Registro")
-                .foregroundColor(Color.white)
-                .padding()
+                //.foregroundColor(Color.white)
+                //.padding()
         
         
     }
@@ -214,18 +159,7 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-/*Branches
- //1.- Branch 01 (pantallaModulos): UI de primer parte de la pantalla de iniciar sesion (Imagen logo + botones "Iniciar sesion" y "Registro" )
- 
- //2.- Branch 02 (pantallaInicioSesion): UI de pantalla de Inicio de Sesión completa
- 
- 
- 
- 
- 
- 
- 
- */
+
 
 
 
